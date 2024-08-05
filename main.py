51730af87440345a7cf3f4f70585daae18a2a1e7
kashincounter <bebra7275@gmail.com>
2024-08-05 16:05:15 +0300

    ..

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("C:\Users\N_OFFICE_5\PycharmProjects\Socol2024\Blum - Google Chrome 2024-08-05 11-12-09.mp4")
+cap = cv2.VideoCapture("C://Users/N_OFFICE_5\PycharmProjects\Socol2024\Blum - Google Chrome 2024-08-05 11-12-09.mp4")
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 
 
 
