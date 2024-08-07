f07cec3f16e93ed30a0f1fa57b78c98766868ec3
kashincounter <bebra7275@gmail.com>
2024-08-07 11:40:39 +0300

    ..

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("C:\Users\N_OFFICE_5\PycharmProjects\Socol2024\Blum - Google Chrome 2024-08-05 11-12-09.mp4")
+first = "C://Users/N_OFFICE_5/PycharmProjects/Socol2024/Blum - Google Chrome 2024-08-05 11-12-09.mp4"
+new = 'C://Users/N_OFFICE_5/PycharmProjects/Socol2024/NewBlum.mp4'
+cap = cv2.VideoCapture(first)
+
+if not cap.isOpened():
+    print("Unable to open camera")
+
+else:
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    '''
+    print(width, type(width))
+    print(height, type(height))
+    print(fps, type(fps))
+    '''
+    outputfile = cv2.VideoWriter(new, fourcc, fps, (width, height))
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        else:
+            nframe = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            lgb = np.array([0, 0, 0])
+            hgb = np.array([0, 0, 0])
+            green_mask = cv2.inRange(frame, lgb, hgb)
+            lbb = np.array([0, 0, 0])
+            hbb = np.array([0, 0, 0])
+            blue_mask = cv2.inRange(frame, lbb, hbb)
+            leb = np.array([0, 0, 0])
+            heb = np.array([0, 0, 0])
+            explosive_mask = cv2.inRange(frame, leb, heb)
+            x = cv2.findContours(nframe, )
+
+
+
+
+
 
 
 
